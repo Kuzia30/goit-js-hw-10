38120732323810d13320e387e3fdef71859c635b
Kuzia30 <maxzimka37@gmail.com>
2021-11-12 13:47:40 +0200

    finish logic

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
+import articleTplList from './templates/countries_list.hbs';
+import articleTpl from './templates/country_template.hbs';
 import './css/styles.css';
-import  debounce  from "lodash.debounce";
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
 
-import { fetchCountries } from "./js/fetchCountries";
-
+import { fetchCountries } from './js/fetchCountries';
 
 const textEl = document.querySelector('#search-box');
+const listEl = document.querySelector('.country-list');
 
-
-
-textEl.addEventListener('input', debounce(onCountriesFetch, DEBOUNCE_DELAY))
+textEl.addEventListener('input', debounce(onCountriesFetch, DEBOUNCE_DELAY));
 function onCountriesFetch(evt) {
-    const array = fetchCountries(evt.target.value);
+    const inputValue = evt.target.value.trim();
+    if (inputValue.length === 0) {
+        listEl.innerHTML = '';
+        return  
+    } else {
+  fetchCountries(inputValue)
+    .then(appendCountriesMarkup)
+    .catch(error => Notify.failure('Oops, there is no country with that name'))
+    }
 }
+
+function appendCountriesMarkup(countries) {
+      if (countries.length > 9) {
+        return Notify.info('Too many matches found. Please enter a more specific name.');
+      }
+      if (countries.length === 1) {
+        countries.forEach(country => (listEl.innerHTML = articleTpl(country)));
+      } else {
+        listEl.innerHTML = articleTplList(countries);
+      }
+}
+
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,12 +1,17 @@
 
+
 export function fetchCountries(name) {
-     fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
-        .then(response => response.json())
-        .then(countries => countries.forEach(({name, capital, population, flags, languages}) => console.log(capital)))
-        .catch(error => console.log(error))
+   return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+       .then(handleResponse)
     }
 
-// () => console.log(capital)
+function handleResponse (response) {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response.json();
+};
+
 
 
 
